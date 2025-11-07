d8b7dc4ded4685e9988e2945ec04d32ea55c3dfb
kpsarvesh <kpsarvesh@outlook.com>
2025-11-07 23:57:33 +0530

    provider_block_updated

--- todoapp_infra/provider.tf
+++ todoapp_infra/provider.tf
@@ -5,8 +5,15 @@ terraform {
       version = "3.85.0"
     }
   }
+    backend "azurerm" {
+    resource_group_name  = "devops_rg"
+    storage_account_name = "skdevstg"
+    container_name       = "sktfstate"
+    key                  = "tfstate"
+  }
 }
-
 provider "azurerm" {
   features {}
+  subscription_id = "3c5a5847-bba8-4a7e-b700-87684df57784"
+  skip_provider_registration = true
 }
